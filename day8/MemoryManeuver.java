9a375590c572b2efcacc475c992df0af524910f8
StanleyMines <stanleymines@gmail.com>
2018-12-07 21:21:14 -0800

    Day 8: Cleaned up the little needed, and commented for readability.

--- day8/MemoryManeuver.java
+++ day8/MemoryManeuver.java
@@ -6,60 +6,74 @@ public class MemoryManeuver
 {
   public static void main(String[] args)
   {
-    FileInput fi = new FileInput("src/main/java/aoc2018/day8/input");
-    System.out.println("Part 1: " + readO(fi));
-    fi.close();
-
-    Node root = new Node(new FileInput("src/main/java/aoc2018/day8/input"));
+    TreeNode root = new TreeNode(new FileInput("src/main/java/aoc2018/day8/input"));
+    System.out.println("Part 1: " + root.part1Count());
     System.out.println("Part 2: " + root.part2Count());
   }
 
-  private static int readO(FileInput fi)
+  public static class TreeNode
   {
-    int subNodes = fi.readInt();
-    int metaCount = fi.readInt();
-    int ret = 0;
-    for (int i = 0; i<subNodes; i++)
-      ret += readO(fi);
-    for (int i = 0; i<metaCount; i++)
-      ret += fi.readInt();
-    return ret;
-  }
-
-  public static class Node
-  {
-    Node[] children;
+    TreeNode[] children;
     int[] meta;
-    public Node(FileInput fi)
+
+    /**
+     * Read in from the file.
+     *
+     * @param fi
+     */
+    public TreeNode(FileInput fi)
     {
-      children = new Node[fi.readInt()];
+      // Read header (# children and # metadata)
+      children = new TreeNode[fi.readInt()];
       meta = new int[fi.readInt()];
-      for (int i = 0; i<children.length; i++)
-        children[i] = new Node(fi);
-      for (int i = 0; i<meta.length; i++)
+
+      // Read in children (by recursively calling this)
+      for (int i = 0; i < children.length; i++)
+        children[i] = new TreeNode(fi);
+
+      // Read in meta
+      for (int i = 0; i < meta.length; i++)
         meta[i] = fi.readInt();
     }
+
+    /**
+     * Sum the meta and the children's meta.
+     */
     public int part1Count()
     {
       int ret = 0;
-      for (Node aChildren : children)
+
+      // Sum childrens' meta sums (recursively)
+      for (TreeNode aChildren : children)
         ret += aChildren.part1Count();
+
+      // Sum the meta
       for (int val : meta)
         ret += val;
+
       return ret;
     }
+
+    /**
+     * If no children, sum the meta
+     * else, recursively call this for the children, of the indexes given by the meta.
+     */
     public int part2Count()
     {
       int ret = 0;
+
+      // No children -> sum meta
       if (children.length == 0)
         for (int val : meta)
           ret += val;
+
+      // If children -> recursively call this for indexes determined by meta.
       else
         for (int val : meta)
           if (val >= 1 && val <= children.length)
             ret += children[val - 1].part2Count();
-      return ret;
 
+      return ret;
     }
   }
 }
