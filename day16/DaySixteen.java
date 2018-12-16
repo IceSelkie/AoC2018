be0e0d7afac28d1b3c40d41479f45d7a5a0ddd2f
StanleyMines <stanleymines@gmail.com>
2018-12-15 22:52:32 -0800

    Day 16: Cleaned code.

--- day16/DaySixteen.java
+++ day16/DaySixteen.java
@@ -6,71 +6,48 @@ import util.Util;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.ListIterator;
 
 public class DaySixteen
 {
-  public static final String[] opcodes = new String[] {"addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtrr", "gtri", "gtir", "eqrr", "eqri", "eqir"};
+  public static final String[] opcodes = new String[]{"addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtrr", "gtri", "gtir", "eqrr", "eqri", "eqir"};
+
   public static void main(String[] args)
   {
-    ArrayList<int[][]> info = readAllSets(new FileInput("src/main/java/aoc2018/day16/inputPart1Clean"));
+    ArrayList<int[][]> input1 = readAllSets(new FileInput("src/main/java/aoc2018/day16/inputPart1Clean"));
 
-    HashMap<Integer,ArrayList<String>> hm = new HashMap<>();
-    for (int i = 0; i<16; i++)
-      hm.put(i,Util.toArrayListString(opcodes));
+    HashMap<Integer, ArrayList<String>> possibleOpcodesMap = new HashMap<>();
+    for (int i = 0; i < 16; i++)
+      possibleOpcodesMap.put(i, Util.toArrayListString(opcodes));
 
-    int ct = 0;
-    for (int[][] data : info)
+    int countOverThree = 0;
+    for (int[][] data : input1)
     {
-      //System.out.println(++ct+" "+ Util.aTS(possibilities(data).toArray()));
-      ArrayList<String> posibs = possibilities(data);
-      ArrayList<String> knownPosibs = hm.remove(data[1][0]);
-      posibs.removeIf(s -> !knownPosibs.contains(s));
-      hm.put(data[1][0], posibs);
-      if (posibs.size() >= 3)
-        ct++;
+      ArrayList<String> possibilitiesFromThisRun = possibilities(data);
+      if (possibilitiesFromThisRun.size() >= 3)
+        countOverThree++;
+      possibilitiesFromThisRun.removeIf(s -> !possibleOpcodesMap.get(data[1][0]).contains(s));
+      possibleOpcodesMap.replace(data[1][0], possibilitiesFromThisRun);
     }
-    System.out.println("Part 1: " + ct);
+    System.out.println("Part 1: " + countOverThree);
 
-    boolean noChange = false;
-    while (!noChange)
-    {
-      noChange = true;
-      for (int i = 0; i < 16; i++)
-        if (hm.get(i).size() == 1)
-          for (int j = 0; j < 16; j++)
-            if (i != j)
-              if (hm.get(j).remove(hm.get(i).get(0)))
-                noChange = false;
-    }
+    String[] reference = new String[16];
+    int opcodesKnown = 0;
 
-    HashMap<Integer, String> reference = new HashMap<>(16);
-    for (int i = 0; i<16; i++)
-    {
-      ArrayList<String> onlyPosib = hm.get(i);
-      if (onlyPosib.size() == 1)
-      {
-        System.out.println("Opcode " + i + " is " + onlyPosib.get(0));
-        reference.put(i, onlyPosib.get(0));
-      }
-    }
-
-    for (int[][] data : info)
-      if (!Arrays.equals(data[2],doOp(reference.get(data[1][0]),data[0],data[1])))
-      System.out.println("Error w/ op "+data[1][0]);
+    while (opcodesKnown < 16)
+      for (int k = 0; k < 16; k++)
+        if (possibleOpcodesMap.containsKey(k) && possibleOpcodesMap.get(k).size() == 1 && opcodesKnown++ > -1)
+        {
+          reference[k] = possibleOpcodesMap.remove(k).get(0);
+          for (Integer k2 : possibleOpcodesMap.keySet())
+            possibleOpcodesMap.get(k2).remove(reference[k]);
+        }
 
     ArrayList<int[]> input2 = readAll(new FileInput("src/main/java/aoc2018/day16/inputPart2"));
-    int[] regist = new int[4];
-    for (int i = 0; i<input2.size(); i++)
-    {
-      int[] instSet = input2.get(i);
-      try
-      {
-        regist = doOp(reference.get(instSet[0]), regist, instSet);
-      } catch (IndexOutOfBoundsException e) {/*e.printStackTrace();*/ System.err.println("Input: "+Util.aTS(regist));System.err.println("Instruct "+i+": ("+reference.get(instSet[0])+") "+Util.aTS(instSet));}
-    }
+    int[] register = new int[4];
+    for (int[] instructionSet : input2)
+      register = runOpcode(reference[instructionSet[0]], register, instructionSet);
 
-    System.out.println("Part 2: " + Util.aTS(regist));
+    System.out.println("Part 2: " + register[0]);
   }
 
   public static ArrayList<String> possibilities(int[][] dataset)
@@ -80,7 +57,7 @@ public class DaySixteen
     {
       try
       {
-        int[] output = doOp(op, dataset[0], dataset[1]);
+        int[] output = runOpcode(op, dataset[0], dataset[1]);
         if (Arrays.equals(output, dataset[2]))
           ret.add(op);
       } catch (ArrayIndexOutOfBoundsException e) {}
@@ -88,58 +65,47 @@ public class DaySixteen
     return ret;
   }
 
-  private static int[] doOp(String op, int[] input, int[] instructions)
+  private static int[] runOpcode(String opcode, int[] register, int[] instructions)
   {
-    int[] ret = new int[4];
-    for (int i = 0; i<4; i++)
-      ret[i]=input[i];
+    int[] registerFinal = new int[4];
+    System.arraycopy(register, 0, registerFinal, 0, 4);
 
     boolean binary = true;
-    if (op.startsWith("gt")||op.startsWith("eq"))
+    if (opcode.startsWith("gt") || opcode.startsWith("eq"))
       binary = false;
 
-    boolean r1= op.substring(0,binary?3:2).equals("set")?op.charAt(3) == 'r':(binary || op.charAt(2) == 'r');
-    boolean r2= op.charAt(3)=='r';
+    boolean r1 = opcode.startsWith("set") ? opcode.charAt(3) == 'r' : (binary || opcode.charAt(2) == 'r');
+    boolean r2 = opcode.charAt(3) == 'r';
 
-    int outReg = instructions[3];
-    int vala = r1?input[instructions[1]]:instructions[1];
-    int valb = r2?input[instructions[2]]:instructions[2];
-    int out = -1;
+    int value_A = r1 ? register[instructions[1]] : instructions[1];
+    int value_B = r2 ? register[instructions[2]] : instructions[2];
 
-
-    op = op.substring(0,binary?3:2);
-    switch (op)
+    opcode = opcode.substring(0, binary ? 3 : 2);
+    switch (opcode)
     {
       case "add":
-        out = vala+valb;
+        registerFinal[instructions[3]] = value_A + value_B;
         break;
       case "mul":
-        out = vala*valb;
+        registerFinal[instructions[3]] = value_A * value_B;
         break;
       case "ban":
-        out = vala&valb;
+        registerFinal[instructions[3]] = value_A & value_B;
         break;
       case "bor":
-        out = vala|valb;
+        registerFinal[instructions[3]] = value_A | value_B;
         break;
       case "set":
-        out = vala;
+        registerFinal[instructions[3]] = value_A;
         break;
       case "gt":
-        out = vala>valb?1:0;
+        registerFinal[instructions[3]] = value_A > value_B ? 1 : 0;
         break;
       case "eq":
-        out = vala==valb?1:0;
+        registerFinal[instructions[3]] = value_A == value_B ? 1 : 0;
         break;
-      default:
-        outReg=-1;
-        System.err.println("Op code not found.");
     }
-    if (outReg!=-1)
-      ret[outReg] = out;
-
-
-    return ret;
+    return registerFinal;
   }
 
   public static ArrayList<int[][]> readAllSets(FileInput fi)
@@ -149,13 +115,15 @@ public class DaySixteen
       ret.add(readSet(fi));
     return ret;
   }
+
   public static int[][] readSet(FileInput fi)
   {
     int[][] ret = new int[3][];
-    for (int i = 0; i<3; i++)
+    for (int i = 0; i < 3; i++)
       ret[i] = read4(fi);
     return ret;
   }
+
   public static ArrayList<int[]> readAll(FileInput fi)
   {
     ArrayList<int[]> ret = new ArrayList<>();
@@ -163,10 +131,11 @@ public class DaySixteen
       ret.add(read4(fi));
     return ret;
   }
+
   public static int[] read4(FileInput fi)
   {
     int[] ret = new int[4];
-    for (int i = 0; i<4; i++)
+    for (int i = 0; i < 4; i++)
       ret[i] = fi.readInt();
     return ret;
   }
